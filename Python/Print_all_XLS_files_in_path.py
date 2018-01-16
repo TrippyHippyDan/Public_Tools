9b7a058701a1fecba808169f05bba7dd2bd529e0
TrippyHippyDan <dfellenz@nextiva.com>
2018-01-16 13:36:27 -0700

    Staring a program to pull all XLS files from a directory

---
+++ Python/Print_all_XLS_files_in_path.py
@@ -0,0 +1,8 @@
+import os
+
+#Note this was built for using python and git bash on windows
+
+dirpath = 'Users\xxxx\Downloads' # this is an example pathway
+
+os.chdir('C:\\' + dirpath)
+os.system('ls -h')
