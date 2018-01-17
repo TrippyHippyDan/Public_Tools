ac7386b3a9816ddb7d4cd36a3804bd0ab68a0d80
Daniel Fellenz <dfellenz@nextiva.com>
2018-01-17 11:05:05 -0700

    Was able to pull all files and print a list of files.

--- Python/Print_all_XLS_files_in_path.py
+++ Python/Print_all_XLS_files_in_path.py
@@ -1,8 +1,28 @@
+# Note this was built for using python and git bash on windows
+# Made by Daniel Fellenz 01/17/2018
+# Imports for the files
 import os
 
-#Note this was built for using python and git bash on windows
+allfiles = []
 
-dirpath = 'Users\xxxx\Downloads' # this is an example pathway
+def PullFiles(wPath, fType):
+    workingpath = wPath
+    subpaths = []
+    filelist = []
+    print('Searching in ' + wPath + ' and all sub dirs')
+    for fname in os.listdir(wPath):
+        fullName = os.path.join(wPath, fname)
+        if os.path.isfile(fullName):
+            if fullName.endswith(fType):
+                filelist.append(fname)
+                allfiles.append(fname)
+                print(fullName)
+                print('Found file: ' + fname)
+        elif os.path.isdir(fullName):
+            subpaths.append(fullName)
+    for subpath in subpaths:
+        PullFiles(subpath, fType)
+    print(filelist)
 
-os.chdir('C:\\' + dirpath)
-os.system('ls -h')
+PullFiles('C:\Program Files (x86)\Steam\steamapps\common\Sid Meier\'s Civilization V\Assets\DLC\Expansion2', '.xml')
+print(allfiles)
